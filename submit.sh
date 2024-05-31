9861abb7ad5c4ab828532d4c0335fddaaa4bcfed
cordutie <egutierreo@uc.cl>
2024-05-31 21:51:44 +0200

    new submit

--- submit.sh
+++ submit.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-#SBATCH -J create_env_ddsp_textures
-#SBATCH -p short
+#SBATCH -J ddsp_textures_training
+#SBATCH -p high
 #SBATCH -N 1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=8g
-#SBATCH --time=1:00:00
+#SBATCH --mem=96g
+#SBATCH --time=2:00:00
 #SBATCH -o %N.job%J.log_output.txt
 #SBATCH -e %N.job%J.log_errors.txt
 
-echo "### Creating conda environment 'ddsp_textures'..."
-conda create -y -n ddsp_textures python=3.8
+echo "### Loading modules..."
+module --ignore-cache load CUDA
+module load Python tqdm
+echo "### ...done."
 
-echo "### Activating conda environment 'ddsp_textures'..."
-source activate ddsp_textures
-
-echo "### Installing packages..."
-conda install -y -c pytorch torch torchaudio librosa numpy scipy
-pip install tqdm matplotlib
-
-echo "### Verifying installed packages..."
-conda list
+echo "### Installing modules..."
+python3 -m pip cache list
+python3 -m pip install --upgrade pip
+python3 -m pip install numpy
+python3 -m pip install librosa
+python3 -m pip install torch
+python3 -m pip install torchinfo
+echo "### ...done."
 
 echo "### HPC Job properties:"
 echo "Number of Nodes Allocated     : $SLURM_JOB_NUM_NODES"
 echo "Number of Tasks Allocated     : $SLURM_NTASKS"
 echo "Number of Cores/Task Allocated: $SLURM_CPUS_PER_TASK"
 
-echo "###### Environment 'ddsp_textures' creation finished ######"
+echo "### Starting script 'main.py' ... $(date)"
+python3 main.py
+echo "###### Finished ###### $(date)"
